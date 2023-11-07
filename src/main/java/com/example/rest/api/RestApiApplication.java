a0678548eb1bc1cf23d49c93daf8e9a9a262516a
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-11-07 17:11:15 +0100

    console out

--- src/main/java/com/example/rest/api/RestApiApplication.java
+++ src/main/java/com/example/rest/api/RestApiApplication.java
@@ -1,13 +1,29 @@
 package com.example.rest.api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApiApplication {
+/*
+ * 
+ * Spring Boot fournit une interface nommée “CommandLineRunner”. En implémentant
+ * cette interface,
+ * la classe sera obligée de déclarer la méthode “public void run(String...
+ * args) throws Exception“.
+ * À partir de là, si la classe est un bean (c’est-à-dire chargée dans le
+ * contexte Spring),
+ * Spring Boot exécutera la méthode run à l’exécution du programme.
+ * 
+ */
+public class RestApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("The console is working.");
+	}
 }
