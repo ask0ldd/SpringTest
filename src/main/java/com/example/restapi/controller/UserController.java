34bd0c9e6afcf75cfb74ace3927c08ff787c0359
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-11-09 02:00:56 +0100

    get user endpoint

--- src/main/java/com/example/restapi/controller/UserController.java
+++ src/main/java/com/example/restapi/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.example.restapi.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+// import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +35,18 @@ public class UserController {
         return userService.getUsers();
     }
 
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable("id") final Long id) {
+        System.out.println(id);
+        Optional<User> user = userService.getUser(id);
+        System.out.println(user);
+        if (user.isPresent()) {
+            return user.get();
+        } else {
+            return null;
+        }
+    }
+
     @PostMapping("/user")
     public User createUser(@RequestBody User user) {
         return userService.saveUser(user);
@@ -46,3 +61,88 @@ public class UserController {
      */
 
 }
+
+/*
+ * package com.openclassrooms.api.controller;
+ * 
+ * import java.util.Optional;
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired;
+ * import org.springframework.web.bind.annotation.DeleteMapping;
+ * import org.springframework.web.bind.annotation.GetMapping;
+ * import org.springframework.web.bind.annotation.PathVariable;
+ * import org.springframework.web.bind.annotation.PostMapping;
+ * import org.springframework.web.bind.annotation.PutMapping;
+ * import org.springframework.web.bind.annotation.RequestBody;
+ * import org.springframework.web.bind.annotation.RestController;
+ * 
+ * import com.openclassrooms.api.model.Employee;
+ * import com.openclassrooms.api.service.EmployeeService;
+ * 
+ * @RestController
+ * public class EmployeeController {
+ * 
+ * @Autowired
+ * private EmployeeService employeeService;
+ * 
+ * @PostMapping("/employee")
+ * public Employee createEmployee(@RequestBody Employee employee) {
+ * return employeeService.saveEmployee(employee);
+ * }
+ * 
+ * 
+ * @GetMapping("/employee/{id}")
+ * public Employee getEmployee(@PathVariable("id") final Long id) {
+ * Optional<Employee> employee = employeeService.getEmployee(id);
+ * if(employee.isPresent()) {
+ * return employee.get();
+ * } else {
+ * return null;
+ * }
+ * }
+ * 
+ * @GetMapping("/employees")
+ * public Iterable<Employee> getEmployees() {
+ * return employeeService.getEmployees();
+ * }
+ * 
+ * @PutMapping("/employee/{id}")
+ * public Employee updateEmployee(@PathVariable("id") final Long
+ * id, @RequestBody Employee employee) {
+ * Optional<Employee> e = employeeService.getEmployee(id);
+ * if(e.isPresent()) {
+ * Employee currentEmployee = e.get();
+ * 
+ * String firstName = employee.getFirstName();
+ * if(firstName != null) {
+ * currentEmployee.setFirstName(firstName);
+ * }
+ * String lastName = employee.getLastName();
+ * if(lastName != null) {
+ * currentEmployee.setLastName(lastName);;
+ * }
+ * String mail = employee.getMail();
+ * if(mail != null) {
+ * currentEmployee.setMail(mail);
+ * }
+ * String password = employee.getPassword();
+ * if(password != null) {
+ * currentEmployee.setPassword(password);;
+ * }
+ * employeeService.saveEmployee(currentEmployee);
+ * return currentEmployee;
+ * } else {
+ * return null;
+ * }
+ * }
+ * 
+ * @DeleteMapping("/employee/{id}")
+ * public void deleteEmployee(@PathVariable("id") final Long id) {
+ * employeeService.deleteEmployee(id);
+ * }
+ * 
+ * 
+ * }
+ * 
+ * 
+ */
