08943aff8a9a735a081a0642d0af7e56bf4e4d4e
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-11-12 23:18:54 +0100

    endpoints free from spring security

--- src/main/java/com/example/restapi/configuration/SpringSecurityConfig.java
+++ src/main/java/com/example/restapi/configuration/SpringSecurityConfig.java
@@ -2,13 +2,17 @@ package com.example.restapi.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+        // h2-console urls and users endpoint are not protected
+        return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"),
+                new AntPathRequestMatcher("/users/**"), new AntPathRequestMatcher("/user/**"));
     }
 }
