41c82dbc28daaca329fe376104f4a5a070067dae
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-11-14 17:39:58 +0100

    jwt auth filter v2

--- src/main/java/com/example/restapi/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/restapi/filter/JwtAuthenticationFilter.java
@@ -2,7 +2,10 @@ package com.example.restapi.filter;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.example.restapi.service.JwtService;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -11,16 +14,21 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private JwtService jwtService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-                final String authHeader = request.getHeader("Authorization");
-                final String jwt;
-                if(authHeader == null || !authHeader.startsWith("Bearer ")){
-                    filterChain.doFilter(request, response);
-                    return;
-                }
-                jwt = authHeader.substring(7);
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUserEmail(jwt);
     }
 
 }
