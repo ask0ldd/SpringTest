2afd1fe25fc4a45582424ae5b5c9cfaa4961f156
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-11-15 05:13:17 +0100

    app config

---
+++ src/main/java/com/example/restapi/configuration/ApplicationConfig.java
@@ -0,0 +1,29 @@
+package com.example.restapi.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.example.restapi.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepository.findByEmail(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("User Not Found."));
+            }
+        };
+    }
+}
